a44a28b79b0e300011676a4002b369d9e169d4cb
Alessandra Souza <alessandrasilvasouza@live.com>
2018-12-06 18:30:42 -0200

    wip menu responsive

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import './scss/main.scss'
 
-const Button = document.querySelector(".btn");
+const button = document.querySelector(".btn");
 const inputPhone = document.getElementById("phone");
 const divError = document.querySelector(".erro");
-Button.addEventListener("click", function (event) {
+
+const mobile = document.querySelector(".nav__menu_mobile");
+const menuItens = document.querySelector(".nav__menu_itens");
+
+mobile.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (menuItens.style.top === "60px") {
+    menuItens.style.top= "-60px"; 
+  } else {
+    menuItens.style.top= "60px"; 
+  }
+})
+
+button.addEventListener("click", function (event) {
   event.preventDefault();
   const phone = inputPhone.value;
   if (!phone.trim()) {
@@ -18,7 +31,6 @@ Button.addEventListener("click", function (event) {
     divError.innerHTML = "Digite um telefone válido!";
     return false;
   } else {
-    // const phone = phone.replace(/\D/g, "");
     window.open(`https://api.whatsapp.com/send?l=pt_br&phone=55${phone}`)
   } 
 })
@@ -26,33 +38,3 @@ Button.addEventListener("click", function (event) {
 function inRange(x, min, max) {
   return ((x-min)*(x-max) <= 0);
 }
-
-// function mtel(v){
-//   v=v.replace(/\D/g,"");             //Remove tudo o que não é dígito
-//   v=v.replace(/^(\d{2})(\d)/g,"($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
-//   v=v.replace(/(\d)(\d{4})$/,"$1-$2");    //Coloca hífen entre o quarto e o quinto dígitos
-//   return v;
-// }
-
-// function mascara(object,f){
-//   v_obj=object
-//   v_fun=f
-//   setTimeout("execmascara()",1)
-// }
-// function execmascara(){
-//   v_obj.value=v_fun(v_obj.value)
-// }
-// function mtel(v){
-//   v=v.replace(/\D/g,"");             //Remove tudo o que não é dígito
-//   v=v.replace(/^(\d{2})(\d)/g,"($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
-//   v=v.replace(/(\d)(\d{4})$/,"$1-$2");    //Coloca hífen entre o quarto e o quinto dígitos
-//   return v;
-// }
-// function id( el ){
-// return document.getElementById( el );
-// }
-// window.onload = function(){
-// id('telefone').onkeyup = function(){
-//   mascara( this, mtel );
-// }
-// }
